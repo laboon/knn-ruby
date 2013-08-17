c8e2cac794b02a3e8cc6b0619d783efddb67e50d
laboon <laboon@gmail.com>
2013-08-17 13:56:39 -0400

    added comments

--- knn.rb
+++ knn.rb
@@ -1,15 +1,29 @@
+# A simple demonstration of the k-nearest neighbors algorithm.
+# By default, using 3 attributes (thus, three dimensions).
+# This can easily be changed by modifying the data at the bottom of this file.
+# Usage: ruby knn.rb <k> <attribute_1> <attribute_2> <attribute_3>
+# e.g. ruby knn.rb 3 4 5 6
+# will classify based on 3 closest neighbors to a point at 4,5,6.
+# Missing: any sort of error-checking or -handling whatsoever.
+
+# Determine Euclidean distance between two points, a and b
+# This will work for an arbitrary number of dimensions
 def euclidean_distance(a, b)
   Math.sqrt ( a.zip(b).map { |n| n.reduce(:-) }.map { |n| n**2 }.reduce(:+) )
 end
 
+# Determine the k closest neighbors to a point
 def closest_neighbors(num_neighbors, point, data)
   data.keys.sort { |a,b| euclidean_distance(a, point) <=> euclidean_distance(b, point) }.take(num_neighbors)
 end
 
+# Determine the classification of an input based on the largest number of elements
+# of the k closest neighbors
 def classification(num_neighbors, point, data)
   closest_neighbors(num_neighbors, point, data).map { |p| data[p] }.group_by { |n| n }.max_by(&:size).first
 end
 
+# Read in data from the bottom of the file
 def read_data
   entities = Hash.new
   DATA.each_line.map(&:chomp).each do |entity|
@@ -18,8 +32,10 @@ def read_data
   entities
 end
 
+# Main execution
 p classification(ARGV[0].to_i, ARGV.slice(1..-1).map { |n| n.to_i }, read_data)
 
+# Data (3 dimensions)
 __END__
 1,1,1,elf
 1,2,1,elf
